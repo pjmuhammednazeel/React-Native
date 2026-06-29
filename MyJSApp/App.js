ad1bc2ba31c017fb4169185e4c621aad31e96eb3
pjmuhammednazeel <nazeelnazeel57@gmail.com>
2026-06-29 13:26:04 +0530

    added multiple components

--- MyJSApp/App.js
+++ MyJSApp/App.js
@@ -1,11 +1,41 @@
 import { View, Text } from "react-native";
 
+function Name(){
+  return(
+    <Text>P J Muhammed Nazeel</Text>
+  );
+}
+function About(){
+  return(
+    <Text>I am Learning React Native</Text>
+  );
+}
+function Student(){
+  return(
+    <Text>Student:Nazeel</Text>
+  );
+}
+function Course(){
+  return(
+    <Text>Course:Mca</Text>
+  );
+}
+function College(){
+  return(
+    <Text>College:Muthoot Institute Of Technology And Sciences</Text>
+  )
+}
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text style={{ color: "black" }}>
         Hello
       </Text>
+      <Name/>
+      <About/>
+      <Student/>
+      <Course/>
+      <College/>
     </View>
   );
 }
