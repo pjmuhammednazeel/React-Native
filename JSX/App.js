a9f65d24edb069c9c5383b726c69bc23436b2893
pjmuhammednazeel <nazeelnazeel57@gmail.com>
2026-07-02 12:56:21 +0530

    Jsx Basics

--- JSX/App.js
+++ JSX/App.js
@@ -6,6 +6,8 @@ export default function App(){
     <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
     <Text>HAi</Text>
     <Curly/>
+    <Exp/>
+    <Info/>
     </View>
   );
 }
@@ -18,4 +20,28 @@ function Curly(){
   return(
     <Text>Name:{name}</Text>
   );
+}
+
+//Use of Expression
+
+function Exp(){
+  const a = 10;
+  const b = 20;
+  return(
+    <Text>{a+b}</Text>
+
+  );
+}
+
+//Use of objects
+
+function Info(){
+  const users={
+    name:"Nazeel",
+    Roll:"46",
+  };
+  return(
+    <Text>{users.name}</Text>
+  );
+
 }
